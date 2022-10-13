60624a412657b0f880f544fa4476a34838219160
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-10-13 19:29:00 -0300

    tentando criar o script

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/*function acessarPerfil(){
+    perfil.style.display = "block"
+    res_busca.style.display = "none"
+}
+
+function voltarBusca(){
+    perfil.style.display = "none"
+    res_busca.style.display = "block"
+}*/
+
+for(let i = 0; i < 3; i++){
+    let icon = document.getElementsByName("icon")[i]
+    icon.innerHTML = "ok meu bom"
+}
