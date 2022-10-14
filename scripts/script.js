68b7b1fa52759ceeb66d7e54d72eb1340f2ab73e
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-10-14 19:28:59 -0300

    exibição dos perfis

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,34 @@
-/*function acessarPerfil(){
+let perfils = {
+    perfil1: {
+        nome: "Micael",
+        imagem: "imagens/i1.jpg",
+    },
+
+    perfil2:{
+        nome: "Mikael",
+        imagem: "imagens/i2.jpg", 
+    },
+
+    perfil3:{
+        nome:"Michael",
+        imagem: "imagens/i3.jpg",
+    }
+}
+
+
+function acessarPerfil(k){
     perfil.style.display = "block"
     res_busca.style.display = "none"
+    imagem_perfil.setAttribute("src", perfils[k].imagem)
+    nome.innerHTML = perfils[k].nome
 }
 
 function voltarBusca(){
     perfil.style.display = "none"
     res_busca.style.display = "block"
-}*/
+}
 
 for(let i = 0; i < 3; i++){
-    let icon = document.getElementsByName("icon")[i]
-    icon.innerHTML = "ok meu bom"
+    let icon = document.getElementsByClassName("icon")[i]
+    icon.style.backgroundImage = `url(imagens/i${i+1}.jpg)`
 }
